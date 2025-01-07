cdf9c8987cdb4fb41ac489375ad7f0a08c1eedf9
Milena Genez <genezmilena@gmail.com>
2025-01-07 14:01:25 -0300

    añadir componentes básicos

--- tienda-angular/src/app/app.component.ts
+++ tienda-angular/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListadoProductosComponent } from './listado-productos/listado-productos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ListadoProductosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tienda-angular';
+  title = 'Tienda Angular - Componentes';
 }

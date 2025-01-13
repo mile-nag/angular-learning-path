d577724659b18c88f5df779135311719a7ede762
Milena Genez <genezmilena@gmail.com>
2025-01-13 12:01:13 -0300

    agrego componente y la operación

--- calculadora-app/src/app/app.component.ts
+++ calculadora-app/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalculadoraComponent } from "./calculadora/calculadora.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CalculadoraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

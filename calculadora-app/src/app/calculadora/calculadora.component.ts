1810192d4aefa4273805a897b600ebe1b772fff5
Milena Genez <genezmilena@gmail.com>
2025-02-18 13:04:44 -0300

    decorador output, decorador input

--- calculadora-app/src/app/calculadora/calculadora.component.ts
+++ calculadora-app/src/app/calculadora/calculadora.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FormularioComponent } from '../formulario/formulario.component';
+import { ResultadoComponent } from "../resultado/resultado.component";
 
 @Component({
   selector: 'app-calculadora',
-  imports: [FormsModule],
+  imports: [FormsModule, FormularioComponent, ResultadoComponent],
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.css',
 })
 export class CalculadoraComponent {
-  operandoA: number = 0;
-  operandoB: number = 0;
   resultado: number = 0;
 
-  sumar() {
-    this.resultado = this.operandoA + this.operandoB
+  procesarResultado(resultado: number) {
+    this.resultado = resultado;
   }
 }
---
+++ calculadora-app/src/app/formulario/formulario.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario',
+  imports: [FormsModule],
+  templateUrl: './formulario.component.html',
+  styleUrl: './formulario.component.css'
+})
+export class FormularioComponent {
+  operandoA: number | null = null;
+  operandoB: number | null = null;
+
+  @Output() resultadoSuma = new EventEmitter<number>()
+  
+  sumar() {
+    if(this.operandoA && this.operandoB){
+      const resultado = this.operandoA + this.operandoB
+      this.resultadoSuma.emit(resultado)
+    }
+    else{
+      console.log('Se deben introducir valores correctos.')
+    }
+  }
+}

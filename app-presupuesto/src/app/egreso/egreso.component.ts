17a2f5ec7eaf7be98fb889aa0ee88b9fe8d43a84
Milena Genez <genezmilena@gmail.com>
2025-02-24 13:51:40 -0300

    plantilla con bootstrap

---
+++ app-presupuesto/src/app/egreso/egreso.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-egreso',
+  imports: [],
+  templateUrl: './egreso.component.html',
+  styleUrl: './egreso.component.css'
+})
+export class EgresoComponent {
+
+}

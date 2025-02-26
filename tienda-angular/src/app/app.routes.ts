5a81c8097beee5ac2da1daf1c14ded6a84c09400
Milena Genez <genezmilena@gmail.com>
2025-02-26 14:56:24 -0300

    Agregar componente error

--- tienda-angular/src/app/app.routes.ts
+++ tienda-angular/src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { ListadoProductosComponent } from './listado-productos/listado-productos.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
 
   {path: '', component: ListadoProductosComponent}, //localhost:4200
   {path: 'listado', component: ListadoProductosComponent}, //localhost:4200/listado
   {path: 'agregar', component: FormularioComponent}, //localhost:4200/agregar
-  {path:'editar/:id', component: FormularioComponent} //localhost:4200/editar:id}
+  {path:'editar/:id', component: FormularioComponent}, //localhost:4200/editar:id}
+  {path: '**', component: ErrorComponent} //Para manejar el error de una ruta no encontrada
+
 ];

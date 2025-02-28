0746fcb9cea8bf03ae6ee1788a63edcf5a39cd91
Milena Genez <genezmilena@gmail.com>
2025-02-28 15:50:57 -0300

    feat: Agregar componente de login con formulario básico
    - Crear componente de login.
    - Configurar formulario con campos de email y contraseña.
    - Agregar ruta de login en app.routes.ts.

--- tienda-angular/src/app/app.routes.ts
+++ tienda-angular/src/app/app.routes.ts
@@ -2,13 +2,13 @@ import { Routes } from '@angular/router';
 import { ListadoProductosComponent } from './listado-productos/listado-productos.component';
 import { FormularioComponent } from './formulario/formulario.component';
 import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-
-  {path: '', component: ListadoProductosComponent},
-  {path: 'listado', component: ListadoProductosComponent}, 
-  {path: 'agregar', component: FormularioComponent}, 
-  {path:'editar/:key', component: FormularioComponent}, 
-  {path: '**', component: ErrorComponent} //Para manejar el error de una ruta no encontrada
-
+  { path: '', component: ListadoProductosComponent },
+  { path: 'listado', component: ListadoProductosComponent },
+  { path: 'agregar', component: FormularioComponent },
+  { path: 'editar/:key', component: FormularioComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: ErrorComponent }, //Para manejar el error de una ruta no encontrada
 ];
